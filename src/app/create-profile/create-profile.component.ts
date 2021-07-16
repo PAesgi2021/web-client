4d00ab3d674c19fb1de480f9023458b6e3fac94e
ahmedine <krapulxx@gmail.com>
2021-07-16 22:19:20 +0200

    create profile fix

--- src/app/create-profile/create-profile.component.ts
+++ src/app/create-profile/create-profile.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {ProfileService} from "../services/profile-service/profile.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-create-profile',
@@ -18,21 +19,27 @@ export class CreateProfileComponent implements OnInit {
 
 
   constructor(
-    public profileService: ProfileService
+    public profileService: ProfileService,
+    public router: Router,
   ) { }
 
   ngOnInit(): void {
+    this.createProfile();
   }
 
   createProfile() {
-    console.log(this.profileForm.get('pseudo').value)
+    if(this.profileForm.invalid) {
+      return;
+    }
 
-    this.profileService.createProfile({
-      pseudo: "155",
-      firstName: "string",
-      lastName: "123",
+    this.profileService.createProfile( {
+      pseudo: this.profileForm.get('pseudo').value,
+      firstName: this.profileForm.get('firstname').value,
+      lastName: this.profileForm.get('lastname').value,
       account_id: 1
     }).subscribe();
+    this.router.navigate(['/profile-management'])
   }
+
 
 }
